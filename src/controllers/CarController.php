a6399967ec35371eb2adfc3c9494b78440fe27fd
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-10-31 20:12:34 +0000

    read JSON with cars

--- src/controllers/CarController.php
+++ src/controllers/CarController.php
@@ -5,9 +5,14 @@ class CarController {
 
   public function __construct()
   {
-    $this->cars[] = new Car("2453","Ford", "Kuga", 2021, "blue");
-    $this->cars[] = new Car("4687","Ford", "Puma", 2020, "red");
-    $this->cars[] = new Car("1231","Seat", "Ibiza", 2012, "blue");
+    // $this->cars[] = new Car("2453","Ford", "Kuga", 2021, "blue");
+    // $this->cars[] = new Car("4687","Ford", "Puma", 2020, "red");
+    // $this->cars[] = new Car("1231","Seat", "Ibiza", 2012, "blue");
+    $json = file_get_contents('../data/cars.json');
+    $carsJSON = json_decode($json);
+    foreach ($carsJSON as $carJSON) {
+      $this->cars[] = new Car($carJSON->id,$carJSON->make,$carJSON->model,$carJSON->year,$carJSON->color);
+    }
   }
 
   public function list() {
